8139dde51de2129946171d78f9c55690495e67f3
Katherine Fancher-Johnes <kljohnes@gmail.com>
2021-08-26 01:29:14 -0400

    completed most of challenge

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -3,16 +3,19 @@ import './PokeFetch.css';
 
 
 class PokeFetch extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
-    }
+      seconds: 10
+    };
   }
 
   fetchPokemon() {
+    
+
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
@@ -20,27 +23,62 @@ class PokeFetch extends Component {
       method: 'GET'
     }).then(res => res.json())
       .then(res => {
+        
+        
         this.setState({
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
-          pokeName: res.species.name,
+          pokeName: res.species.name
+          
         })
       })
+      
       .catch((err) => console.log(err))
+  
+
+    
+  
+    this.myInterval = setInterval(() => {
+        const { seconds } = this.state
+
+        if (seconds > 0) {
+            this.setState(({ seconds }) => ({
+                seconds: seconds - 1
+            }))
+        }
+
+    }, 1000)
   }
 
+
+// componentWillUnmount(){
+//   clearInterval(this.myInterval)
+
+// };
+  
   render() {
+    const { seconds } = this.state
     return (
+      
       <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
-        <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
-        </div>
+        <button className={'start'} onClick={() => this.fetchPokemon()} >Start!</button>
+       
+        <h1 className={'timer'} >   {  seconds === 0
+                    ? <div className={'pokeWrap'}>
+                    <img className={'pokeImg'} src={this.state.pokeSprite} />
+                    <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+                  </div>
+                    : <div><h1>Time Remaining: :{seconds < 10 ? `0${seconds}` : seconds}</h1> <div className={'pokeWrap'}>
+                    <img className={'pokeImgHide'} src={this.state.pokeSprite} />
+                    <h1 className={'pokeNameHide'}>{this.state.pokeName}</h1>
+                  </div></div>
+                }</h1>
+       
       </div>
     )
   }
 }
+
+
 
 export default PokeFetch;
